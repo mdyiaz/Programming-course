a807c55c96be9626f50b74f78219ebebcf0ac933
mdyiaz <mdyiaz@gmail.com>
2022-10-26 22:11:41 +0600

    login form created

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-        
+            <form className='	' action="">
+
+                    <p className='font-bold	'>Name:</p>
+                    <input type="text" placeholder="Type your name" className="input input-bordered input-primary w-full max-w-xs my-5" /> <br />
+
+                    <p className='font-bold'>Email:</p>
+                    <input type="email" placeholder="Type your email" className="input input-bordered input-primary w-full max-w-xs my-5" /> <br />
+
+
+                    <p className='font-bold'>Password:</p>
+                    <input type="password" placeholder="Type your password" className="input input-bordered input-primary w-full max-w-xs my-5 " />
+
+
+            </form>
         </div>
     );
 };
